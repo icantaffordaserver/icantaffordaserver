d24a12fd1dc75ad008ee06b92ca45915abe070b8
Alexander Mann <alexander.mann@me.com>
2016-12-27 13:38:26 -0500

    update app routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,13 @@ import {
     inviteDelete,
     updateInvitePut
 } from '../controllers/invites';
+import {
+    allConnectionsGet,
+    newConnectionPost,
+    connectionGet,
+    updateConnectionPut,
+    connectionDelete
+} from '../controllers/connections';
 
 routes.post('/contact', contactController.contactPost);
 
@@ -38,7 +45,26 @@ routes.get('/auth/facebook/callback', userController.authFacebookCallback);
 routes.get('/invites', allInvitesGet); // get all invites
 routes.post('/invites', newInvitePost); // create an invite
 // routes.get('/invites/:id/accept', acceptInviteGet); // TODO: accept an invite and then be able to sign up
-// routes.put('/invites/:id', updateInvitePut); // TODO: update an invite (resend, change email, etc)
+routes.put('/invites/:id', updateInvitePut); // TODO: update an invite (resend, change email, etc)
 routes.delete('/invites/:id', inviteDelete); // delete an invite
+
+/**
+ * Connections Routes
+ */
+routes.get('/connections', allConnectionsGet);
+routes.get('/connections/:id', connectionGet);
+routes.post('/connections', newConnectionPost);
+routes.put('/connections/:id', updateConnectionPut);
+routes.delete('/connections/:id', connectionDelete);
+
+/**
+ * Connection Progress Routes
+ */
+// TODO: wire up these routes and configure webhooks
+routes.get('/connections/progress');
+routes.get('/connections/progress');
+routes.post('/connections/progress');
+routes.put('/connections/progress');
+routes.delete('/connections/progress');
 
 module.exports = routes;
