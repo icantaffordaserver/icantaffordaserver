09fa9c76a6c0757d45ac7d192cd0876fa5fd17e4
Alexander Mann <alexander.mann@me.com>
2017-01-04 10:53:45 -0500

    add test database environment

--- knexfile.js
+++ knexfile.js
@@ -4,17 +4,29 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    client: 'pg',
-    connection: process.env.DATABASE_URL || {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+    development: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL || {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        },
+        migrations: {
+            directory: __dirname + '/server/data/migrations'
+        },
+        seeds: {
+            directory: __dirname + '/server/data/seeds'
+        }
     },
-    migrations: {
-        directory: __dirname + '/server/data/migrations'
-    },
-    seeds: {
-        directory: __dirname + '/server/data/seeds'
+    test: {
+        client: 'pg',
+        connection: 'postgres://localhost:5432/testing',
+        migrations: {
+            directory: __dirname + '/server/data/migrations'
+        },
+        seeds: {
+            directory: __dirname + '/server/data/seeds'
+        }
     }
 };
--- server/config/bookshelf.js
+++ server/config/bookshelf.js
@@ -1,4 +1,5 @@
-const config        = require('../../knexfile');
+const environment   = process.env.NODE_ENV;
+const config        = require('../../knexfile')[environment];
 const knex          = require('knex')(config);
 const bookshelf     = require('bookshelf')(knex);
 const cascadeDelete = require('bookshelf-cascade-delete');
