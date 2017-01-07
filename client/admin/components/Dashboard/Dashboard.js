4ba0e715f631b0861b1f9963d67083ad56e2a87e
Steven Eng <steven_eng@live.ca>
2017-01-06 17:24:29 -0800

    Use Sent By name + allow resend invite in Invites Sent Panel

--- client/admin/components/Dashboard/Dashboard.js
+++ client/admin/components/Dashboard/Dashboard.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import InvitesSent from './InvitesSent';
 import SendInvite from './SendInvite';
+import Messages from '../Messages';
 
 class Dashboard extends React.Component {
   render() {
     return (
       <div className="container-fluid">
+        <Messages messages={this.props.messages} />
         <div className="col-sm-6">
         </div>
         <div className="col-sm-6">
@@ -21,4 +24,10 @@ class Dashboard extends React.Component {
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    messages: state.messages
+  }
+};
+
+export default connect(mapStateToProps)(Dashboard);
