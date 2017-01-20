5ad77191e12fc1dc04cb92eaba25a24900124932
Alexander Mann <alexander.mann@me.com>
2017-01-20 18:38:07 -0500

    Fixes #25 - Changing front end routing to make sure users can only signup with a valid, unaccepted invite

--- client/admin/routes.js
+++ client/admin/routes.js
@@ -44,7 +44,7 @@ export default function getRoutes(store) {
             </Route>
             <Route path="contact" component={Contact} onLeave={clearMessages}/>
             <Route path="login" component={Login} onEnter={skipIfAuthenticated} onLeave={clearMessages}/>
-            <Route path="signup" component={Signup} onEnter={skipIfAuthenticated} onLeave={clearMessages}/>
+            <Route path="signup/invite/:inviteId" component={Signup} onEnter={skipIfAuthenticated} onLeave={clearMessages}/>
             <Route path="account" component={Profile} onEnter={ensureAuthenticated} onLeave={clearMessages}/>
             <Route path="forgot" component={Forgot} onEnter={skipIfAuthenticated} onLeave={clearMessages}/>
             <Route path='reset/:token' component={Reset} onEnter={skipIfAuthenticated} onLeave={clearMessages}/>
