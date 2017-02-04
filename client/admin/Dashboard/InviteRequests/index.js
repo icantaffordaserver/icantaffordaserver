3b2297a3f61baf41c6dda88591823a0c412ca2a0
Steven Eng <steven_eng@live.ca>
2017-02-03 22:51:01 -0800

    Fixes 37 - Make Invite Requests panel style consistent

--- client/admin/Dashboard/InviteRequests/index.js
+++ client/admin/Dashboard/InviteRequests/index.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
-import { Header, Button, Checkbox, Icon, Table } from 'semantic-ui-react';
+import { Header, Button, Checkbox, Icon, Table, Segment } from 'semantic-ui-react';
 import { fetchInviteRequests, toggleInviteRequestCheckbox, approveInvites } from './actions';
 
 const POLL_INTERVAL = 10000;
@@ -47,50 +47,52 @@ class InviteRequests extends React.Component {
   render() {
     return (
       <div>
-        <Header as="h2">Invite Requests</Header>
-        <Table compact celled definition>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell />
-              <Table.HeaderCell>Name</Table.HeaderCell>
-              <Table.HeaderCell>Date Requested</Table.HeaderCell>
-              <Table.HeaderCell>E-mail address</Table.HeaderCell>
-              <Table.HeaderCell>Heard From What Channel</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-
-          <Table.Body>
-            {this.props.inviteRequests.all.map((inviteRequest, index) => !inviteRequest.invite_id && (
+        <Header as="h2" attached="top">Invite Requests</Header>
+        <Segment attached>
+          <Table compact celled definition basic="very">
+            <Table.Header>
               <Table.Row>
-                <Table.Cell collapsing>
-                  <Checkbox
-                    checked={this.props.inviteRequests.checkedRequests.includes(inviteRequest.id)}
-                    onChange={this.handleInviteRequestCheckbox.bind(this, inviteRequest.id)}
-                  />
-                </Table.Cell>
-                <Table.Cell>{inviteRequest.first_name} {inviteRequest.last_name}</Table.Cell>
-                <Table.Cell>{moment(inviteRequest.created_at).format('MMM Do, YYYY')}</Table.Cell>
-                <Table.Cell>{inviteRequest.email}</Table.Cell>
-                <Table.Cell>TBD</Table.Cell>
+                <Table.HeaderCell />
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Date Requested</Table.HeaderCell>
+                <Table.HeaderCell>E-mail address</Table.HeaderCell>
+                <Table.HeaderCell>Heard From What Channel</Table.HeaderCell>
               </Table.Row>
-            ))}
-          </Table.Body>
-          <Table.Footer fullWidth>
-            <Table.Row>
-              <Table.HeaderCell />
-              <Table.HeaderCell colSpan="4">
-                <Button
-                  disabled={this.props.inviteRequests.checkedRequests < 1}
-                  onClick={this.handleApproveInviteButton.bind(this, this.props.inviteRequests.checkedRequests)}
-                  floated="right" icon labelPosition="left" primary size="small"
-                >
-                  <Icon name="user" />
-                  {this.props.inviteRequests.checkedRequests.length > 1 ? 'Approve Invite Requests' : 'Approve Invite Request'}
-                </Button>
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Footer>
-        </Table>
+            </Table.Header>
+
+            <Table.Body>
+              {this.props.inviteRequests.all.map((inviteRequest, index) => !inviteRequest.invite_id && (
+                <Table.Row>
+                  <Table.Cell collapsing>
+                    <Checkbox
+                      checked={this.props.inviteRequests.checkedRequests.includes(inviteRequest.id)}
+                      onChange={this.handleInviteRequestCheckbox.bind(this, inviteRequest.id)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>{inviteRequest.first_name} {inviteRequest.last_name}</Table.Cell>
+                  <Table.Cell>{moment(inviteRequest.created_at).format('MMM Do, YYYY')}</Table.Cell>
+                  <Table.Cell>{inviteRequest.email}</Table.Cell>
+                  <Table.Cell>TBD</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+            <Table.Footer fullWidth>
+              <Table.Row>
+                <Table.HeaderCell />
+                <Table.HeaderCell colSpan="4">
+                  <Button
+                    disabled={this.props.inviteRequests.checkedRequests < 1}
+                    onClick={this.handleApproveInviteButton.bind(this, this.props.inviteRequests.checkedRequests)}
+                    floated="right" icon labelPosition="left" primary size="small"
+                  >
+                    <Icon name="user" />
+                    {this.props.inviteRequests.checkedRequests.length > 1 ? 'Approve Invite Requests' : 'Approve Invite Request'}
+                  </Button>
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Footer>
+          </Table>
+        </Segment>
       </div>
     );
   }
