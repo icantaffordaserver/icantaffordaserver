3c8ee99f1cb724fe0c8f82930fd91f771f43b074
Alexander Mann <alexander.mann@me.com>
2017-02-14 22:43:38 -0500

    Update Message component to support multiple errors

--- client/shared/Messages/index.js
+++ client/shared/Messages/index.js
@@ -5,18 +5,27 @@ class Messages extends React.Component {
   render() {
     return this.props.messages.success ? (
         <Message success>
-          {this.props.messages.success.map((message, index) =>
-            <div key={index}>{message.msg}</div>)}
+          <Message.List>
+            {this.props.messages.success.map((message, index) =>
+              <Message.List key={index}>{message.msg}</Message.List>
+            )}
+          </Message.List>
         </Message>
       ) : this.props.messages.error ? (
           <Message error list={this.props.messages.error}>
-            {this.props.messages.error.map((message, index) =>
-              <div key={index}>{message.msg}</div>)}
+            <Message.List>
+              {this.props.messages.error.map((message, index) =>
+                <Message.Item key={index}>{message.msg}</Message.Item>
+              )}
+            </Message.List>
           </Message>
         ) : this.props.messages.info ? (
             <Message warning>
-              {this.props.messages.info.map((message, index) =>
-                <div key={index}>{message.msg}</div>)}
+              <Message.List>
+                {this.props.messages.info.map((message, index) =>
+                  <Message.Item key={index}>{message.msg}</Message.Item>
+                )}
+              </Message.List>
             </Message>
           ) : null;
   }
