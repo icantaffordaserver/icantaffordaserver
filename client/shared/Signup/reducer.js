b3e278068b8fd2859dd4e5b3cb6f8bc6fce1cfff
Alexander Mann <alexander.mann@me.com>
2017-02-16 21:19:00 -0500

    Migration to create-react-app structure for easier build of the client when ready to push to production, skip server-side rendering for now

--- client/shared/Signup/reducer.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by alexandermann on 2017-01-20.
- */
--- server/index.js
+++ server/index.js
@@ -1,68 +1,33 @@
 import express from 'express';
-import path from 'path';
 import logger from 'morgan';
-import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import expressValidator from 'express-validator';
-import dotenv from 'dotenv';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Router, { RouterContext, match } from 'react-router';
-import { Provider } from 'react-redux';
-import exphbs from 'express-handlebars';
 import jwt from 'jsonwebtoken';
-import moment from 'moment';
-import request from 'request';
-import sass from 'node-sass-middleware';
-import webpack from 'webpack';
 import remoteDev from 'remotedev-server';
-import config from '../webpack.config.babel';
+import dotenv from 'dotenv';
+import serverRoutes from './routes';
 
 // Load environment variables from .env file
-dotenv.load();
+dotenv.config({ path: process.env.PWD + '/.env' });
 
 // Models
 const UserAccounts = require('./models/UserAccounts').UserAccounts;
 
-import serverRoutes from './routes';
-
-// React and Server-Side Rendering
-import routes from '../client/routes';
-import configureStore from '../client/configureStore';
-
 const app = express();
 
-const compiler = webpack(config);
+app.set('port', process.env.PORT || 3001);
 
-const hbs = exphbs.create({
-  defaultLayout: 'main',
-  helpers: {
-    ifeq(a, b, options) {
-      if (a === b) {
-        return options.fn(this);
-      }
-      return options.inverse(this);
-    },
-    toJSON(object) {
-      return JSON.stringify(object);
-    },
-  },
-});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(compression());
-app.use(sass({ src: path.join(__dirname, '..', 'public'), dest: path.join(__dirname, '..', 'public') }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator()); // for asserting and checking submission data, must be after bodyParser middleware
 app.use(cookieParser());
 
 // authentication middleware
-app.use(async (req, res, next) => {
-    // attach an isAuthenticated function to the req object
-  req.isAuthenticated = function () {
+app.use(async(req, res, next) => {
+  // attach an isAuthenticated function to the req object
+  req.isAuthenticated = () => {
     const token = (req.headers.authorization && req.headers.authorization.split(' ')[1]) || req.cookies.token;
     try {
       return jwt.verify(token, process.env.TOKEN_SECRET);
@@ -71,7 +36,7 @@ app.use(async (req, res, next) => {
     }
   };
 
-    // if user is authenticated, attach user account to the req object
+  // if user is authenticated, attach user account to the req object
   if (req.isAuthenticated()) {
     const payload = req.isAuthenticated();
     const user = await new UserAccounts({ id: payload.sub }).fetch({ withRelated: 'profile' });
@@ -85,47 +50,14 @@ app.use(async (req, res, next) => {
 // Configure middleware for development environment
 if (app.get('env') === 'development') {
   app.use(logger('dev')); // use the 'dev' morgan configuration for logging HTTP requests
-  app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath,
-  }));
-  app.use(require('webpack-hot-middleware')(compiler));
   remoteDev({ hostname: 'localhost', port: 8000 });
 }
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.use('/', serverRoutes);
-
-// React server rendering
-app.use((req, res) => {
-  const initialState = {
-    auth: { token: req.cookies.token, user: req.user },
-    messages: {},
-  };
-
-  const store = configureStore(initialState);
-
-  match({ routes: routes(store), location: req.url }, (err, redirectLocation, renderProps) => {
-    if (err) {
-      res.status(500).end(err.message);
-    } else if (redirectLocation) {
-      res.status(302).redirect(redirectLocation.pathname + redirectLocation.search);
-    } else if (renderProps) {
-      const html = renderToString(
-        <Provider store={store}>
-          <RouterContext {...renderProps} />
-        </Provider>,
-            );
-      res.render('layouts/main', {
-        html,
-        initialState: store.getState(),
-      });
-    } else {
-      res.status(404).end('Not Found.');
-    }
-  });
-});
 
 // Production error handler
 if (app.get('env') === 'production') {
@@ -135,4 +67,8 @@ if (app.get('env') === 'production') {
   });
 }
 
-module.exports = app;
+
+app.listen(app.get('port'), (err) => {
+  if (err) throw err;
+  console.log(`Current Environment: ${app.get('env')}\nExpress server listening on port ${app.get('port')}\n`); // eslint-disable-line no-console
+});
