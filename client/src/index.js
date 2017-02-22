fe3dbb87e544247ff0a66edf6ca22c16366c1980
Alexander Mann <alexander.mann@me.com>
2017-02-22 16:30:42 -0500

    add apollo and begin scaphold setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 import App from './App';
 import configureStore from './configureStore';
 import '../semantic/dist/semantic.min.css';
 
 const store = configureStore();
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({ uri: 'https://us-west-2.api.scaphold.io/graphql/deep-statement' }),
+});
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root'),
 );
