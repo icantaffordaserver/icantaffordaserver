25f0a9f962ab728684c76b5c070700ca9894df59
Alexander Mann <alexander.mann@me.com>
2017-05-05 18:09:25 -0400

    update buildspec deployment for aws

--- serverless/scripts/bundleLambdas.js
+++ serverless/scripts/bundleLambdas.js
@@ -6,9 +6,12 @@ const entryPoints = require('./lambdaEntryPoints')
 
 const lambdas = entryPoints() // get all lambda paths
 
-lambdas.forEach(lambdaDir => { // execute webpack on every lambda function
+lambdas.forEach(lambdaDir => {
+  // execute webpack on every lambda function
   const lambdaName = lambdaDir.split('/').pop()
   shell.echo(`Bundling ${lambdaName}`)
   shell.cd(lambdaDir)
-  shell.exec('../../node_modules/.bin/babel-node ../../scripts/runWebpack.js')
+  shell.exec('../../node_modules/.bin/rimraf lib node_modules') // remove lib and node_modules prior to every build
+  shell.exec('yarn install --prod') // only install dependencies
+  shell.exec('../../node_modules/.bin/babel-node ../../scripts/runWebpack.js') // run webpack on the src
 })
