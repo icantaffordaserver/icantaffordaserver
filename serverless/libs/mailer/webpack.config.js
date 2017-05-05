cf18dfc420db46268ce40b5d45f2c2e5c77c0070
Alexander Mann <alexander.mann@me.com>
2017-05-04 22:36:07 -0400

    use webpack to compile and build production version of the toktumi mailer

---
+++ serverless/libs/mailer/webpack.config.js
@@ -0,0 +1,52 @@
+/**
+ * Created by alexandermann on 2017-05-04.
+ */
+/* global __dirname, require, module*/
+
+const webpack = require('webpack');
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const path = require('path');
+const env  = require('yargs').argv.env; // use --env with webpack 2
+
+let libraryName = 'mailer';
+
+let plugins = [], outputFile;
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  outputFile = libraryName + '.min.js';
+} else {
+  outputFile = libraryName + '.js';
+}
+
+const config = {
+  entry: __dirname + '/src/index.js',
+  target: 'node',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./src')],
+    extensions: ['.json', '.js']
+  },
+  externals: {
+    postmark: 'postmark'
+  },
+  plugins: plugins
+};
+
+module.exports = config;
