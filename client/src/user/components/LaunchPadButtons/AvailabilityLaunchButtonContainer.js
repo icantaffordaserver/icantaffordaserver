0d7933cb2a7f5f3a2532e1fc315c0d816cb00839
Alexander Mann <alexander.mann@me.com>
2017-05-05 20:53:25 -0400

    change AvailabilityContainer text to 'When can you talk?', add disabled helper function to control button state and actions

--- client/src/user/components/LaunchPadButtons/AvailabilityLaunchButtonContainer.js
+++ client/src/user/components/LaunchPadButtons/AvailabilityLaunchButtonContainer.js
@@ -1,50 +1,55 @@
 /**
  * Created by alexandermann on 2017-03-29.
  */
-import React from 'react';
-import { graphql } from 'react-apollo';
-import { withRouter } from 'react-router-dom';
-import CurrentUserQuery from '../../../graphql/user/currentUserQuery';
-import LaunchPadItem from './LaunchPadItem';
-import selection from './057_Selection.png';
+import React from 'react'
+import PropTypes from 'prop-types';
+import { graphql } from 'react-apollo'
+import { withRouter } from 'react-router-dom'
+import CurrentUserQuery from '../../../graphql/user/currentUserQuery'
+import LaunchPadItem from './LaunchPadItem'
+import selection from './057_Selection.png'
 
 class AvailabilityLaunchButtonContainer extends React.Component {
   static propTypes = {
-    data: React.PropTypes.object.isRequired,
-    history: React.PropTypes.object.isRequired,
-  };
+    data: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired,
+  }
 
   handleClick = () => {
-    if (!this.props.data.viewer.user.typeformProfileComplete) return;
-    const { history } = this.props;
-    history.push('/dashboard/availability');
-  };
+    if (this.isDisabled()) return
+    const { history } = this.props
+    history.push('/dashboard/availability')
+  }
 
   hasSetAvailability = () => {
-    if (!this.props.data.viewer.user.availability) return false; // check if availability has been set
-    return Object.keys(this.props.data.viewer.user.availability).length > 0; // return true if availability has 1 slot or more selected
-  };
+    if (!this.props.data.viewer.user.availability) return false // check if availability has been set
+    return Object.keys(this.props.data.viewer.user.availability).length > 0 // return true if availability has 1 slot or more selected
+  }
+
+  isDisabled = () => {
+    const { typeformProfileComplete } = this.props.data.viewer.user
+    return !typeformProfileComplete
+  }
 
   render() {
-    if (this.props.data.loading) return null;
+    if (this.props.data.loading) return null
 
-    const { typeformProfileComplete } = this.props.data.viewer.user;
     const labelMessage = this.hasSetAvailability()
       ? 'Edit your availabilty'
-      : 'You need to set your availability';
+      : 'You need to set your availability'
 
     return (
       <LaunchPadItem
         imgSrc={selection}
-        header="Availability"
+        header="When can you talk?"
         labelColor={this.hasSetAvailability() ? 'green' : 'red'}
         labelMessage={labelMessage}
         labelPosition="top right"
-        disabled={!typeformProfileComplete}
+        disabled={this.isDisabled()}
         onClick={this.handleClick}
       />
-    );
+    )
   }
 }
 
-export default withRouter(graphql(CurrentUserQuery)(AvailabilityLaunchButtonContainer));
+export default withRouter(graphql(CurrentUserQuery)(AvailabilityLaunchButtonContainer))
