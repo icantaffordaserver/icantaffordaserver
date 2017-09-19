3b43ed6280e84c8f748bc42a8d01a1a74daf3ea9
Alexander Mann <alexander.mann@me.com>
2017-09-19 14:37:37 -0400

    add prettier and precommit hook to maintain code style

--- client/src/screens/App/screens/signup1/containers/SignUpContainer1.js
+++ client/src/screens/App/screens/signup1/containers/SignUpContainer1.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { graphql, withApollo, compose } from "react-apollo";
-import { withRouter } from "react-router-dom";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { graphql, withApollo, compose } from 'react-apollo'
+import { withRouter } from 'react-router-dom'
 
-import SignUpForm1 from "../components/SignUpForm1";
+import SignUpForm1 from '../components/SignUpForm1'
 
-import signUpMutation from "../graphql/signUpMutation";
-import signInMutation from "../../../shared/graphql/mutations/signInMutation";
-import currentUserQuery from "../../../shared/graphql/queries/currentUserQuery";
+import signUpMutation from '../graphql/signUpMutation'
+import signInMutation from '../../../shared/graphql/mutations/signInMutation'
+import currentUserQuery from '../../../shared/graphql/queries/currentUserQuery'
 
 class SignUpContainer1 extends Component {
   static propTypes = {
@@ -15,18 +15,16 @@ class SignUpContainer1 extends Component {
     history: PropTypes.object.isRequired,
     client: PropTypes.object.isRequired,
     signUpMutation: PropTypes.func.isRequired,
-    signInMutation: PropTypes.func.isRequired
-  };
+    signInMutation: PropTypes.func.isRequired,
+  }
 
   state = {
     loading: false,
-    error: ""
-  };
+    error: '',
+  }
 
-  handleSignUp = userData => {    
-    this.setState({
-      loading: true
-    });
+  handleSignUp = userData => {
+    this.setState({ loading: true })
 
     // SignUp mutation
     this.props
@@ -36,39 +34,39 @@ class SignUpContainer1 extends Component {
           lastName: userData.lastName,
           email: userData.email,
           password: userData.password,
-          birthday: userData.birthday
-        }
+          birthday: userData.birthday,
+        },
       })
       .then(() =>
         // Sign user in after account creation
         this.props.signInMutation({
           variables: {
             email: userData.email,
-            password: userData.password
-          }
-        })
+            password: userData.password,
+          },
+        }),
       )
       .then(res => {
-        window.localStorage.setItem("auth_token", res.data.signinUser.token);
+        window.localStorage.setItem('auth_token', res.data.signinUser.token)
       })
       .then(() => {
-        this.setState({ loading: false });
+        this.setState({ loading: false })
         // reset the store after the user has been authenticated, then direct to dashboard
-        this.props.client.resetStore();
-        this.props.history.push("/profile");
+        this.props.client.resetStore()
+        this.props.history.push('/profile')
       })
       .catch(error => {
         if (
-          error.message.includes("User already exists with that information")
+          error.message.includes('User already exists with that information')
         ) {
           // check email is not taken
           this.setState({
             loading: false,
-            error: "Email is already associated with an account"
-          });
+            error: 'Email is already associated with an account',
+          })
         }
-      });
-  };
+      })
+  }
 
   render() {
     return (
@@ -77,14 +75,14 @@ class SignUpContainer1 extends Component {
         loading={this.state.loading}
         error={this.state.error}
       />
-    );
+    )
   }
 }
 
 export default compose(
   withRouter,
   withApollo,
-  graphql(signUpMutation, { name: "signUpMutation" }), // name the mutation
-  graphql(signInMutation, { name: "signInMutation" }),
-  graphql(currentUserQuery)
-)(SignUpContainer1);
+  graphql(signUpMutation, { name: 'signUpMutation' }), // name the mutation
+  graphql(signInMutation, { name: 'signInMutation' }),
+  graphql(currentUserQuery),
+)(SignUpContainer1)
