f1dd2ef158a1756c29c391ca9cc3542a447b0da9
Alexander Mann <alexander.mann@me.com>
2017-09-26 22:52:59 -0400

    remove unused deps, update packages

--- serverless/app/routes/userRoutes.js
+++ serverless/app/routes/userRoutes.js
@@ -1,20 +1,17 @@
-import { Router } from "express";
+import { Router } from 'express'
 
-import { sendVerificationEmailHandler } from "../controllers/userControllers/user-subscription-controller";
-import sendPasswordResetEmail from "../logic/createPasswordReset/async/sendPasswordResetEmail";
+import { sendVerificationEmailHandler } from '../controllers/userControllers/user-subscription-controller'
+import sendPasswordResetEmail from '../logic/createPasswordReset/async/sendPasswordResetEmail'
 
-const routes = Router();
+const routes = Router()
 
-const createUser = () => console.log("hi");
+const createUser = () => console.log('hi')
 
 // All microservice functions related to users type
-routes.post("/requestPipeline", createUser);
-routes.use("/schemaExtension", createUser);
+routes.post('/requestPipeline', createUser)
+routes.use('/schemaExtension', createUser)
 
-routes.post(
-  "/subscription/sendVerificationEmail",
-  sendVerificationEmailHandler
-);
-routes.post("/subscription/sendPasswordResetEmail", sendPasswordResetEmail);
+routes.post('/subscription/sendVerificationEmail', sendVerificationEmailHandler)
+routes.post('/subscription/sendPasswordResetEmail', sendPasswordResetEmail)
 
-export default routes;
+export default routes
