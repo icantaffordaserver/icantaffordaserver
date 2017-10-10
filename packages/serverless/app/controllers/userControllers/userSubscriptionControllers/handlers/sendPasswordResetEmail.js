ef442381adfe687ed0efb418ca9d9ae569b916b3
Alexander Mann <alexandermann@Alexanders-MacBook-Pro.local>
2017-10-10 16:38:30 -0400

    refactor repo to use yarn workspaces

---
+++ packages/serverless/app/controllers/userControllers/userSubscriptionControllers/handlers/sendPasswordResetEmail.js
@@ -0,0 +1,32 @@
+/**
+ * Created by alexandermann on 2017-03-27.
+ */
+import { sendPasswordResetEmail } from '../../../../../mailer'
+import { createClient } from '../../../../../../config/GraphQLClient'
+import { getPasswordResetUrl } from '../../../../../helpers'
+
+import getPasswordResetQuery from '../../../../../graphql/queries/getPasswordResetQuery'
+
+export default async (req, res) => {
+  const id = req.body.data.PasswordReset.node.id
+  const client = createClient()
+
+  try {
+    const response = await client.request(getPasswordResetQuery, { id })
+    const token = response.PasswordReset.token
+    const { firstName, email } = response.PasswordReset.user
+    const actionUrl = getPasswordResetUrl(id, token)
+
+    await sendPasswordResetEmail({
+      firstName,
+      recipientEmail: email,
+      actionUrl,
+      operatingSystem: 'temporary',
+      browserName: 'temporary',
+    })
+    res.status(200).send({ message: 'Password Reset email sent.' })
+  } catch (error) {
+    console.error(error)
+    res.status(400).send({ message: 'Email not sent' })
+  }
+}
---
+++ packages/serverless/app/graphql/queries/getUserByEmailQuery.js
@@ -0,0 +1,14 @@
+/**
+ * Created by alexandermann on 2017-04-03.
+ */
+
+export default `
+  query getUser($email: String){
+    User(email: $email){
+        id,
+        firstName,
+        lastName,
+        password
+      }
+    }
+`
