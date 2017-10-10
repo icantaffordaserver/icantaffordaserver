efdb5c33a1da057f43674442fb1927a0a8b7ed61
Alexander Mann <alexandermann@Alexanders-MacBook-Pro.local>
2017-10-10 17:13:43 -0400

    remove old config folder

--- config/graphqlConfig.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by alexandermann on 2017-04-10.
- */
-const config = {
-  scapholdUrl: 'us-west-2.api.scaphold.io/graphql/toktumi',
-  graphcoolUrl: 'https://api.graph.cool/simple/v1/cj3l0oa9r1uab0130s8nti73k',
-};
-
-export default config;
