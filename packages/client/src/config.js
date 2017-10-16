e8fe31fd658eee5f738669f65b272fec2c5d9cbf
Alexander Mann <alexander@toktumi.io>
2017-10-15 22:00:16 -0400

    pull and merge

--- packages/client/src/config.js
+++ packages/client/src/config.js
@@ -4,6 +4,6 @@
 const config = {
   graphqlUrl: 'api.graph.cool/simple/v1/cj3l0oa9r1uab0130s8nti73k',
   subscriptionUrl: 'subscriptions.graph.cool/v1/cj3l0oa9r1uab0130s8nti73k',
-};
+}
 
-export default config;
+export default config
