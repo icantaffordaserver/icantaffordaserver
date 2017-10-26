b1e2c84ee969621e7cc476c2995cc5430f326acb
LibanHassan <Liban.s.Hassan@gmail.com>
2017-10-26 18:38:03 -0400

    Completed PM-60.

--- packages/client/src/screens/App/screens/Talk/components/PostConversation/components/PostConversationComponent.js
+++ packages/client/src/screens/App/screens/Talk/components/PostConversation/components/PostConversationComponent.js
@@ -1,59 +1,99 @@
 import React from 'react'
 
 import {
-  Form,
-  FormGroup,
-  FormButton,
-  FormLabel,
-  FormTextArea,
-} from '../../../../../styles/Forms'
+  Button,
+  TextArea,
+  ColumnContainer,
+  Content,
+  Section,
+  Title,
+  Subheading,
+  Text,
+} from '../../../../../styles'
 
-import { Checkbox } from 'semantic-ui-react'
-
-import ReportComponent from './ReportComponent'
+import Report from '../../Report'
 
 class PostConversationComponent extends React.Component {
-  state = {
-    review: '',
-  }
-  updateField = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    })
-  }
-
   render() {
     return (
-      <div>
-        <h1>Post Conversation</h1>
-
-        <div style={{ width: '80%', margin: 'auto' }}>
-          <Form onSubmit={this.handleReview}>
-            <FormGroup>
-              <FormLabel htmlFor="user-review">
-                How was your conversation with {this.props.user.firstName}
-              </FormLabel>
-              <FormTextArea
-                onChange={this.updateField}
-                placehoder={`Review ${this.props.user.firstName}`}
-                type="text"
+      <ColumnContainer>
+        <Title large>Post Conversation</Title>
+        <Subheading darkGray medium fullWidth>
+          Thanks for using Pluto! We're always working hard to make the platform
+          better for you. Please take a couple minutes to give us some feedback
+          so we can help foster even better conversations!
+        </Subheading>
+        <Content expanded>
+          <Section>
+            <Title left medium fullWidth primary>
+              Technical Feedback
+            </Title>
+            <Section gray inline>
+              <Text left>Was the video quality good?</Text>
+              <input
+                type="checkbox"
+                name="videoSatisfactory"
+                checked={this.props.videoSatisfactory}
+                onChange={this.props.handleChange}
               />
-            </FormGroup>
-            <FormGroup>
-              <Checkbox label="I'd like to talk again" />
-            </FormGroup>
-
-            <FormButton>Submit</FormButton>
-          </Form>
-
-          <div>
-            <ReportComponent
-              onReport={this.handleReport}
-              user={this.props.user}
-            />
-          </div>
-        </div>
-      </div>
+            </Section>
+            <Section darkGray inline>
+              <Text left white>
+                Was the audio quality good?
+              </Text>
+              <input
+                type="checkbox"
+                name="audioSatisfactory"
+                checked={this.props.audioSatisfactory}
+                onChange={this.props.handleChange}
+              />
+            </Section>
+          </Section>
+          <Section>
+            <Title left medium fullWidth primary>
+              Conversation Feedback
+            </Title>
+            <Section gray inline>
+              <Text left>
+                Did you enjoy the conversation with{' '}
+                {this.props.otherUser.firstName}?
+              </Text>
+              <Text>
+                <input
+                  type="checkbox"
+                  name="enjoyedConversation"
+                  checked={this.props.enjoyedConversation}
+                  onChange={this.props.handleChange}
+                />
+              </Text>
+            </Section>
+            <Section darkGray>
+              <Text left white>
+                Do you have any comments for {this.props.otherUser.firstName}?
+              </Text>
+              <TextArea name="comment" onChange={this.props.handleChange} />
+            </Section>
+          </Section>
+          <Section>
+            <Title left medium fullWidth primary>
+              Journal
+            </Title>
+            <Subheading left darkGray fullWidth>
+              This is for your eyes only. Jot down anything from the
+              conversation you'd like to remember.
+            </Subheading>
+            <TextArea name="journalEntry" onChange={this.props.handleChange} />
+          </Section>
+          <Button
+            secondary
+            loading={this.props.loading}
+            onClick={this.props.handleReview}
+          >
+            Submit
+          </Button>
+          <Report button />
+        </Content>
+      </ColumnContainer>
     )
   }
 }
