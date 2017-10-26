50973e47cd90207e3100ba8b6d4659d213c5ae51
Alexander Mann <alexander@toktumi.io>
2017-10-25 22:41:21 -0400

    remove unused dependency

--- packages/serverless/app/index.js
+++ packages/serverless/app/index.js
@@ -6,7 +6,6 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
-import serverless from 'serverless-http'
 
 import routes from './routes'
 
