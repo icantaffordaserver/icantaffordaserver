c4921ee72a5914f6571924726e532ac6992f5f51
Alexander Mann <alexander@toktumi.io>
2017-10-28 17:57:57 -0400

    add eslint-plugin-import

--- packages/serverless/.eslintrc.js
+++ packages/serverless/.eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['eslint:recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier',
+  ],
   plugins: ['prettier'],
   env: {
     es6: true,
