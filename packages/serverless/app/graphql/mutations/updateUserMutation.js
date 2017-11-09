95bb990d4aba1f23ea808e24b9309f281c6ace48
Nisanthan <nisanthannanthakumar@Nisanthans-MacBook-Pro.local>
2017-11-09 14:00:04 -0500

    graphql change from profilePhotoId to profilePhotoUrl

--- packages/serverless/app/graphql/mutations/updateUserMutation.js
+++ packages/serverless/app/graphql/mutations/updateUserMutation.js
@@ -1,6 +1,6 @@
 export default `
-  mutation updateUser ($id: ID!, $password: String, $profilePhoto: ID){
-    updateUser(id: $id, password: $password, profilePhotoId: $profilePhoto){
+  mutation updateUser ($id: ID!, $password: String, $profilePhotoUrl: String){
+    updateUser(id: $id, password: $password, profilePhotoUrl: $profilePhotoUrl){
       id
     }
   }
