d7fa0b504c3a31dc66c77061a60b9a28682af0c7
Nisanthan <nisanthannanthakumar@Nisanthans-MacBook-Pro.local>
2017-11-10 11:55:16 -0500

    FooterComponent + styles

---
+++ packages/client/src/screens/App/shared/components/Footer/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { graphql, compose } from 'react-apollo'
+import { withRouter } from 'react-router-dom'
+
+import isAuthenticated from '../../HoCs/isAuthenticated'
+import isVerified from '../../HoCs/isVerified'
+import currentUserQuery from '../../graphql/queries/currentUserQuery'
+
+import { Footer, Link, Logo } from './styles.js'
+
+import { Page, Row, Column } from 'hedron'
+
+const FooterComponent = props => {
+  return (
+    <Page fluid>
+      <Footer>
+        <Row divisions={12} justifyContent={'space-between'}>
+          <Column fluid mdShift={5} md={1}>
+            <Logo>PLUTO</Logo>
+          </Column>
+          <Column fluid mdShift={2} md={1}>
+            <Link to="/toc">Terms and Conditions</Link>
+          </Column>
+          <Column fluid md={1}>
+            <Link to="/contact">Contact Us</Link>
+          </Column>
+          <Column fluid md={1}>
+            <Link
+              to="/logout"
+              onClick={(e => {
+                e.preventDefault()
+                window.localStorage.removeItem('auth_token')
+                this.props.history.push('/login')
+              }).bind(this)}
+            >
+              Logout
+            </Link>
+          </Column>
+        </Row>
+      </Footer>
+    </Page>
+  )
+}
+
+export default compose(
+  graphql(currentUserQuery),
+  isVerified,
+  isAuthenticated,
+  withRouter,
+)(FooterComponent)
